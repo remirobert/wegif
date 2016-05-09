86dc931fd714e27f0f20f38adbff1073ee48fac4
rémi  <remirobert33530@gmail.com>
2016-05-09 14:22:47 +0800

    add sources

---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '9.0'
+use_frameworks!
+
+pod 'Alamofire'
+
+pod 'RxSwift',    '~> 2.0'
+pod 'RxCocoa',    '~> 2.0'
---
+++ wegif/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wegif
+//
+//  Created by Remi Robert on 09/05/16.
+//  Copyright © 2016 Remi Robert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
